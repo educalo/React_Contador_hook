ec24d9ebbb34be8c0b422829b7c77b66b4953248
educalo <64586573+educalo@users.noreply.github.com>
2024-01-15 13:34:03 +0100

    Add files via upload

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -2,6 +2,23 @@
 import React from 'react';
 import '../hojas-de-estilo/Boton.css';
 
+
+//paso props el objeto paso las propiedades directamente: texto, esBotoDeclic (operador de condición), manejarClic
+/*
+function Boton(props) {
+  //añado una clase con una condición, operador ternario,  dependiendo la funcion del botón
+  //añado el event Listener onClick llamando a la función manejarClic que se pasa como props, esta función viene del componente que renderize el boton, en nuestro caso App.js
+  return (
+    <button
+      className={props.esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+      onClick={props.manejarClic}>
+      {props.texto}
+    </button>
+  );
+}
+*/
+
+
 //paso props el objeto paso las propiedades directamente: texto, esBotoDeclic (operador de condición), manejarClic
 function Boton({ texto, esBotonDeClic, manejarClic }) {
   //añado una clase con una condición, operador ternario,  dependiendo la funcion del botón
